1d3d1d8a9b3b0d3ab653e58431fc96e8fb7ca6ce
jaycbhsn <101217869+jaycbhsn@users.noreply.github.com>
2023-04-24 11:52:27 -0600

    Update tts_sound.py

--- tts_sound.py
+++ tts_sound.py
@@ -1,9 +1,15 @@
-from pygame import mixer
+import pygame
 import keyboard
 
-def play_audio(filename):
-    # load the sound file and play it
-    sound = mixer.Sound(filename)
+def play_audio(filename, volume, rate):
+    print(f"Playing sounds with these settings: Volume: {volume}, Rate: {rate:.1f}")
+
+    # Initialize pygame mixer
+    pygame.mixer.init()
+
+    # Load the audio data and set volume
+    sound = pygame.mixer.Sound(filename)
+    sound.set_volume(volume)
     channel = sound.play()
 
     # define the callback function for keypresses
@@ -14,9 +20,11 @@ def play_audio(filename):
             # unregister the callback function
             keyboard.unhook(stop_sound)
 
-    # register the callback function for keypresses
+    # register the callback function for keypressesrate
     keyboard.hook(stop_sound)
 
     # wait while the sound is playing
     while channel.get_busy():
         pass
+    
+    pygame.mixer.quit()
