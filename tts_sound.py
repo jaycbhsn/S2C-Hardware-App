73bd45197b29bc1c93ead50403d7afb50457f421
jaycbhsn <101217869+jaycbhsn@users.noreply.github.com>
2023-04-25 01:08:47 -0600

    Update tts_sound.py
    added playback functionality, for real

--- tts_sound.py
+++ tts_sound.py
@@ -1,11 +1,15 @@
 import pygame
 import keyboard
+import os
 
 def play_audio(filename, volume, rate):
     print(f"Playing sounds with these settings: Volume: {volume}, Rate: {rate:.1f}")
 
     # Initialize pygame mixer
     pygame.mixer.init()
+
+    if int(rate) != 1:
+        change_rate(filename, rate)
 
     # Load the audio data and set volume
     sound = pygame.mixer.Sound(filename)
@@ -28,3 +32,16 @@ def play_audio(filename, volume, rate):
         pass
     
     pygame.mixer.quit()
+
+def change_rate(filename, rate):
+    output_filename = 'export_audio/audio.wav'
+    # Check if the file exists
+    if not os.path.isfile(filename):
+        print(f"File '{filename}' does not exist.")
+        return
+    
+    # Use ffmpeg to change the audio speed
+    os.system('ffmpeg -i "{}" -filter:a "atempo={}" -codec:a pcm_s16le -ar 44100 -ac 2 "{}"'.format(filename, rate, output_filename))
+    
+    # Overwrite the original file with the processed audio
+    os.replace(output_filename, filename)
